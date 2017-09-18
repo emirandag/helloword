726ccd0eb5ec30fff2748c0026bb93609d2a79a4
iaw26540084 <iaw26540084@h20.informatica.escoladeltreball.org>
2017-09-18 17:54:52 +0200

    1 commit

---
+++ src/org/escoladeltreball/helloworld/Main.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.escoladeltreball.helloworld;
+
+/**
+ * @author iaw26540084
+ *
+ */
+public final class Main {
+
+	public static void main(String [] args) {
+		System.out.println("hello world");
+	}
+}
